7c96b59b7d29c5a4418dd5bede26cfae48362348
estrellafel <felixestrella7@gmail.com>
2022-02-14 17:07:12 -0600

    basic api set-up

--- main.py
+++ main.py
@@ -6,12 +6,29 @@ Main python file for our flask project
 
 from distutils.log import error
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+# Put in your the api_key
+api_key = ''
+headers = {'Authorization': 'Bearer {}'.format(api_key)}
+search_api_url = 'https://api.yelp.com/v3/businesses/search'
+params = {'term': 'food', 
+          'location': 'La Crosse, Wisconsin',
+          'limit': 25}
+
 @app.route("/")
 def home():
+    general_api()
     return render_template('home.html', error = error)
+
+def general_api():
+    response = requests.get(search_api_url, headers=headers, params=params, timeout=10)
+    print(response.status_code)
+    data = response.json() # It is a dictionary
+    print(data.keys()) # Keys are ['businesses', 'total', 'region']
+    print(data['businesses'][0])
 
 if __name__ == "__main__":
     app.run()
