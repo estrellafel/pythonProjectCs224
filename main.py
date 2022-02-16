89ef79327f9fafff5ac98c1468f05e570d6a24b3
estrellafel <felixestrella7@gmail.com>
2022-02-15 20:21:36 -0600

    added a basic dynamic feature

--- main.py
+++ main.py
@@ -5,23 +5,36 @@ Main python file for our flask project
 """
 
 from distutils.log import error
-from flask import Flask, render_template
+from random import randint
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 # Put in your the api_key
-api_key = ''
+api_key = 'Iu6RMASUHPZgnXkTKsopdmBGiQK9ht97MCXkyFX_ciEltMPd3YcleBYZHmMcmlrwSmdJqAf_YOv7-CuzP6L64k1Vo64fLATAqoKVYAGwJ3ddbmQ1YmAzBku_Y9sKYnYx'
 headers = {'Authorization': 'Bearer {}'.format(api_key)}
 search_api_url = 'https://api.yelp.com/v3/businesses/search'
-params = {'term': 'food', 
-          'location': 'La Crosse, Wisconsin',
-          'limit': 25}
+params = {}
 
 @app.route("/")
 def home():
     #general_api()
     return render_template('home.html', error = error)
+
+@app.route('/', methods =["GET", "POST"])
+def getForm():
+    term = request.form.get('foodType')
+    location = request.form.get('location')
+    print(term + " " + location)
+    params['term'] = term
+    params['location'] = location
+    params['limit'] = 50
+    response = requests.get(search_api_url, headers=headers, params=params, timeout=10)
+    data = response.json()
+    randBusiness = data['businesses'][randint(0,len(data['businesses']) - 1)]
+    return render_template('home.html', error = error, name = randBusiness['name'])
+
 
 def general_api():
     response = requests.get(search_api_url, headers=headers, params=params, timeout=10)
