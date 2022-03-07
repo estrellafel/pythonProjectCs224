3ad0d518a59a620c73d64872aced57aa85ebe8a8
JacksonGoth3 <78447410+JacksonGoth3@users.noreply.github.com>
2022-03-07 15:54:29 -0600

    Implemented radius feature (now uses miles)

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def get_form():
     location = request.form.get('location')
     latitude = request.form.get('latitude')
     longitude = request.form.get('longitude')
+    radius = request.form.get('radius')
     # Set params
     params['term'] = term
     if location == '':
@@ -36,9 +37,13 @@ def get_form():
         params['longitude'] = float(longitude)
     else:
         params['location'] = location
+    if radius != None and radius != '':
+        params['radius'] = int(min(max(float(radius) * 1609.34, 0), 40000))
     params['limit'] = 50
     response = requests.get(search_api_url, headers=headers, params=params, timeout=10)
     data = response.json()
+    if data is None or 'total' not in data.keys():
+        return render_template('home.html', error = error, errorMsg = 'error when getting dictionary response from yelp')
     if data['total'] == 0:
         return render_template('home.html', error = error, errorMsg = 'no businesses found')
     randBusiness = data['businesses'][randint(0,len(data['businesses']) - 1)]
