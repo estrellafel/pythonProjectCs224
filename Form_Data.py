6f61f38c0a0cadd763196ffe98d571d6d522e6c1
estrellafel <felixestrella7@gmail.com>
2022-04-25 13:00:11 -0500

    Commented the Form_Data file

--- Form_Data.py
+++ Form_Data.py
@@ -14,32 +14,42 @@ class Form_Data(object):
         self.categories = categories
         self.price = price
 
+    # Will set the term, given the term paramter
     def set_term(self, term):
         self.term = term
     
+    # Will return the term
     def get_term(self):
         return self.term
 
+    # Will set the location, given the location paramter
     def set_location(self, location):
         self.location = location
 
+    # Will return the location
     def get_location(self):
         return self.location
 
+    # Will set the radius, given the radius paramter
     def set_radius(self, radius):
         self.radius = radius
 
+    # Will return the radius
     def get_radius(self):
         return self.radius
 
+    # Will set the categories, given the categories paramter
     def set_categories(self, categories):
         self.categories = categories
 
+    # Will return the categories
     def get_categories(self):
         return self.categories
 
+    # Will set the price, given the price paramter
     def set_price(self, price):
         self.price = price
 
+    # Will return the price
     def get_price(self):
         return self.price
